123dca7b7d35ebf626f0bd630440f19140ac3e91
Victor Rafael <victorsls@outlook.com>
2020-06-02 22:21:01 -0300

    Clearing the code
    Signed-off-by: Victor Rafael <victorsls@outlook.com>

--- backend/meetings/meeting/views.py
+++ backend/meetings/meeting/views.py
@@ -1,4 +1,3 @@
-from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -6,16 +5,8 @@ from meetings.meeting.models import Meeting
 from meetings.meeting.serializers import MeetingSerializer
 
 
-# class MeetingViewSet(viewsets.ModelViewSet):
-#     serializer_class = MeetingSerializer
-#     http_method_names = ['get']
-#
-#     def get_queryset(self):
-#         return Meeting.objects.filter(employees=self.request.user)
-
-
 class MeetingList(APIView):
     def get(self, request):
-        serializer = MeetingSerializer(Meeting.objects.filter(employees=request.user), many=True,
-                                       context={'request': request})
+        queryset = Meeting.objects.filter(employees=request.user)
+        serializer = MeetingSerializer(queryset, many=True, context={'request': request})
         return Response(serializer.data)
